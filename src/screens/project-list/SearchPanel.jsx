751cdcd0fadf819fa7fe5039f703170af48a0260
myonpin_glb <1317463702@qq.com>
2022-04-08 14:22:07 +0800

    feat:添加工程列表

---
+++ src/screens/project-list/SearchPanel.jsx
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+import React from "react";
+
+export const SearchPanel = ({param,setParam,users}) => {
+  
+  
+  return <form>
+    <input type="text" value={param.name} onChange={evt => setParam({
+      ...param,
+      name: evt.target.value
+    })}/>
+    <select value={param.personId} onChange={evt => setParam({
+      ...param,
+      personId: evt.target.value
+    })}>
+      <option value={''}>负责人</option>
+      {users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)}
+    
+    </select>
+  </form>
+}
---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {SearchPanel} from "./SearchPanel";
+import {List} from "./List";
+import {useEffect, useState} from "react";
+import qs from "qs";
+import {cleanObject} from "../../utils";
+
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  });
+  
+  const [list, setList] = useState([]);
+  
+
+  
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => { //函数里面的内容是异步调用的，所以要用async，里面用await去接收
+      if (response.ok) {
+        setList(await response.json());  //这里这样就省去了一个 .then()的会回调去获取值
+      }
+    });
+  }, [param]);
+  
+  useEffect(()=>{
+    fetch(`${apiUrl}/users`).then(async (response) => { //函数里面的内容是异步调用的，所以要用async，里面用await去接收
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  },[])
+  
+  
+  
+  return <div>
+    
+    <SearchPanel param={param} setParam={setParam} users={users}/>
+    <List list={list} users={users}/>
+  </div>
+}
