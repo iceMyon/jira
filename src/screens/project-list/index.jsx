03bfd55c13adf903e38f1d9559af546139a6abe0
myonpin_glb <1317463702@qq.com>
2022-04-08 17:03:39 +0800

    feat:useMount & useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import {SearchPanel} from "./SearchPanel";
 import {List} from "./List";
 import {useEffect, useState} from "react";
 import qs from "qs";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -18,6 +18,7 @@ export const ProjectListScreen = () => {
   
   const [list, setList] = useState([]);
   
+  const debounceParam = useDebounce(param,2000)
 
   
   useEffect(() => {
@@ -26,15 +27,15 @@ export const ProjectListScreen = () => {
         setList(await response.json());  //这里这样就省去了一个 .then()的会回调去获取值
       }
     });
-  }, [param]);
+  }, [debounceParam]);
   
-  useEffect(()=>{
+  useMount(()=>{
     fetch(`${apiUrl}/users`).then(async (response) => { //函数里面的内容是异步调用的，所以要用async，里面用await去接收
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  },[])
+  })
   
   
   
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from "react";
+
 export const isFalsy = (value) => value === 0 ? false : !value
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -10,3 +12,36 @@ export const cleanObject = (object) => {
   })
   return result
 }
+
+// 自定义hook只能在 1.其他hook中运行   2.组件中运行
+// 所以这个自定义hook useMount 要以use开头
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value,delay)=>{
+  const [debounceValue,setDebouncedValue] = useState(value)
+  
+  useEffect(()=>{
+    const timeout = setTimeout(()=> setDebouncedValue(value), delay)    //每次再value变化以后都会设置一个定时器，delay变化的时候也会设置一个定时器
+    return ()=> clearTimeout(timeout)    //每次在上一个useEffect处理完再运行
+  },[value,delay])
+  return debounceValue  // return了一个debounce 但是没有提供set的方法
+}
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return () => {
+//     if (timeout) {
+//       clearTimeout(timeout)
+//     }
+//     timeout = setTimeout(function () {
+//       func()
+//     }, delay)
+//   }
+// }
+// const log = debounce(() => console.log('call'), 5000)
+// log()
+// log()
+// log()
